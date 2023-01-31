c42e194ec893ee1fa2fcdee1b8794f9349a66b15
CS_DEV <christian.sadikmelik@gmail.com>
2023-01-31 09:45:10 +0100

    Coding adozioniJs

---
+++ assets/js/adozioni.js
@@ -0,0 +1,11 @@
+/**
+ * jQuery starts
+ */
+jQuery( document ).ready(function($) {
+
+    // Redirect page after click
+    $(document).on('click', '.post-68252', function (){
+      $(window.location).attr('href', 'http://www.aclodv.org/volontariato/');
+    });
+    
+  });
