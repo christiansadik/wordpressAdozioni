94bf9e6fc3ecb588a6721c5d3b9dd0a46f5722bd
CS_DEV <christian.sadikmelik@gmail.com>
2023-01-31 18:23:35 +0100

    edit comments

--- adozioni.php
+++ adozioni.php
@@ -1,12 +1,12 @@
 <?php
 /**
-Plugin Name: CsDev - Adozioni Script
-Description: Script per la pagina adozioni
-Version: 1.0
-Company: Forbidden Design
-Company URI: https://www.forbidden.design
-Author: Christian Sadik melik
-License: MIT
+* Plugin Name: CsDev - Adozioni Script
+* Description: Script per la pagina adozioni
+* Version: 1.0
+* Company: Forbidden Design
+* Company URI: https://www.forbidden.design
+* Author: Christian Sadik melik
+* License: MIT
 */
 
 /**
