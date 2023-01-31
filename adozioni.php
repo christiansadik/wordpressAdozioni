52532ad1bea802431b6964ae30fdd745446e0b39
CS_DEV <christian.sadikmelik@gmail.com>
2023-01-31 09:40:33 +0100

    included custom scripts

--- adozioni.php
+++ adozioni.php
@@ -1,3 +1,4 @@
+<?php
 /**
 Plugin Name: CsDev - Adozioni Script
 Description: Script per la pagina adozioni
@@ -7,3 +8,28 @@ Company URI: https://www.forbidden.design
 Author: Christian Sadik melik
 License: MIT
 */
+
+/**
+ * This function includes scripts js and custom styles
+ */
+function enqueue_custom_scripts(){
+
+    
+    if (is_singular('adozioni') == true || is_post_type_archive( 'adozioni' ) == true) {
+        wp_enqueue_script('boostrap-min-jq', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+            wp_enqueue_script('boostrap-min-js', WP_PLUGIN_URL . '/adozioni/assets/js/bootstrap.min.js');
+            wp_enqueue_style('boostrap-css', WP_PLUGIN_URL . '/adozioni/assets/css/bootstrap.css');
+            wp_enqueue_style('boostrap-theme-css', WP_PLUGIN_URL . '/adozioni/assets/css/bootstrap-theme.css');
+    }
+
+    
+    if( is_front_page() )
+    {
+        wp_enqueue_script('adozioni-js', WP_PLUGIN_URL . '/adozioni/assets/js/adozioni.js');
+    }
+
+    
+};
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
+
+?>
