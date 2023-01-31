b9d19363554fe891f14a7c625351c522c44e42da
CS_DEV <christian.sadikmelik@gmail.com>
2023-01-31 09:50:17 +0100

    Include bootstrap js framework css

--- adozioni.php
+++ adozioni.php
@@ -28,7 +28,6 @@ function enqueue_custom_scripts(){
         wp_enqueue_script('adozioni-js', WP_PLUGIN_URL . '/adozioni/assets/js/adozioni.js');
     }
 
-    
 };
 add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 
